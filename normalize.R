3ba038e99dd2adb64e1374497a899f2d53a8fccc
zhouwei76 <zhouwei7676@hunnu.edu.cn>
2025-04-08 20:57:05 +0800

    normalize.R

--- normalize.R
+++ normalize.R
@@ -1,18 +1,13 @@
-#if (!requireNamespace("BiocManager", quietly = TRUE))
-#    install.packages("BiocManager")
-#BiocManager::install("limma")
 
-
-#引用包
 library(limma)
 
-inputFile="geneMatrix.txt"     #表达数据文件
-conFile="s1.txt"               #对照组的样品信息文件
-treatFile="s2.txt"             #实验组的样品信息文件
-geoID="GSE131761"                #GEO数据库研究的id
-setwd("C:\\Users\\zhouw\\Desktop\\sepsis-pathway\\05.normalize\\GSE131761")      #设置工作目录
+inputFile="geneMatrix.txt"     
+conFile="s1.txt"               
+treatFile="s2.txt"           
+geoID="GSE131761"                
+setwd("C:\\Users\\zhouw\\Desktop\\sepsis-pathway\\05.normalize\\GSE131761")      
 
-#读取输入文件，并对输入文件整理
+
 rt=read.table(inputFile, header=T, sep="\t", check.names=F)
 rt=as.matrix(rt)
 rownames(rt)=rt[,1]
@@ -21,7 +16,7 @@ dimnames=list(rownames(exp),colnames(exp))
 data=matrix(as.numeric(as.matrix(exp)),nrow=nrow(exp),dimnames=dimnames)
 rt=avereps(data)
 
-#如果数据没有取log2, 会对数据自动取log2
+
 qx=as.numeric(quantile(rt, c(0, 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
 LogC=( (qx[5]>100) || ( (qx[6]-qx[1])>50 && qx[2]>0) )
 if(LogC){
@@ -29,7 +24,6 @@ if(LogC){
 	rt=log2(rt+1)}
 data=normalizeBetweenArrays(rt)
 
-#读取样品信息的文件(对照组和实验组)
 sample1=read.table(conFile, header=F, sep="\t", check.names=F)
 sample2=read.table(treatFile, header=F, sep="\t", check.names=F)
 sampleName1=gsub("^ | $", "", as.vector(sample1[,1]))
@@ -40,7 +34,7 @@ data=cbind(conData,treatData)
 conNum=ncol(conData)
 treatNum=ncol(treatData)
 
-#输出所有基因矫正后的表达量
+
 Type=c(rep("Control",conNum),rep("Treat",treatNum))
 outData=rbind(id=paste0(colnames(data),"_",Type),data)
 write.table(outData,file=paste0(geoID,".normalize.txt"),sep="\t",quote=F,col.names=F)
